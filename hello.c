07bfb22de18df78eb74a3d8fdf4282bfee41f041
이성은 <katrina060208@gmail.com>
2025-08-19 22:22:14 +0900

    코드 수정(10+20=30)

--- hello.c
+++ hello.c
@@ -4,7 +4,10 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	printf("Hello, Sookmyung Electronics!/n");
+	int a, b; //정수 변수 선언 
+	a = 10;
+	b = 20;
+	printf("%d + %d = %d\n", a, b, a+b);
 	system("pause");
 	return 0;
 }
